92f7ec4795962548a73e5ce7908f745c6b88ed61
Asmee-tech <asmee.khimani@gmail.com>
2025-11-29 10:22:13 +0100

    Implement background loading and scaling function
    Added background function to load and scale images.

--- recycle_game.py
+++ recycle_game.py
@@ -12,11 +12,19 @@ run=True
 score=0
 start=time.time()
 font=pygame.font.SysFont("pressure",20)
-fontt=pygame.font.Sysfont("opera",15)
+fontt=pygame.font.SysFont("opera",15)
 bg=pygame.image.load("recycle_bg.jpg")
+clock=pygame.time.Clock()
+#bg function
+def bgd(img):
+    loaded=pygame.image.load(img)
+    img1=pygame.transform.scale(loaded,(width,height))
+    screen.blit(img1,(0,0))
 while run:
+    clock.tick(30)
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             run=False
+    bgd(bg)
     pygame.display.update()
 pygame.quit()
