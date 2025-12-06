c9a011cdeb24cdf02a677500f00b06353b47d3ea
Asmee-tech <asmee.khimani@gmail.com>
2025-12-06 10:58:09 +0100

    Implement recyclable and non-recyclable item classes
    Added classes for recyclable and non-recyclable items, and updated item generation logic.

--- recycle_game.py
+++ recycle_game.py
@@ -22,12 +22,40 @@ class bin(pygame.sprite.Sprite):
         self.image=pygame.image.load("bin.png")
         self.image=pygame.transform.scale(self.image,(150,140))
         self.rect=self.image.get_rect()
+#cLass for recycleable items
+class rec(pygame.sprite.Sprite):
+    def __init__(self,img):
+        super().__init__()
+        self.image=pygame.image.load(img)
+        self.image=pygame.transform.scale(self.image,(80,80))
+        self.rect=self.image.get_rect()
+        
 #creating bin object
-binimg=bin()
+recimg=["glass.png","paperbag.png","woodenbox.png"]
+recgrp=pygame.sprite.Group()
+nonrecgrp=pygame.sprite.Group()
 imgall=pygame.sprite.Group()
-imgall.add(binimg)
+for i in range(30):
+    objrec=rec(random.choice(recimg))
+    objrec.rect.x=random.randint(10,950)
+    objrec.rect.y=random.randint(10,730)
+    recgrp.add(objrec)
+    imgall.add(objrec)
 #class for non-recycleable items
-
+class nonrec(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image=pygame.image.load("plasticbag.png")
+        self.image=pygame.transform.scale(self.image,(80,80))
+        self.rect=self.image.get_rect()
+for i in range(15):
+    nonobj=nonrec()
+    nonobj.rect.x=random.randint(10,950)      
+    nonobj.rect.y=random.randint(10,730) 
+    nonrecgrp.add(nonobj)
+    imgall.add(nonobj)
+binimg=bin()
+imgall.add(binimg)
 #bg function
 def bgd(img):
     loaded=pygame.image.load(img)
@@ -38,21 +66,30 @@ while run:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             run=False
-    bgd("recycle_bg.jpg")
-    #bin movement
-    keys=pygame.key.get_pressed()
-    if keys[pygame.K_DOWN]:
-        #if binimg.rect.y>=0:
-            binimg.rect.y+=10
-    if keys[pygame.K_UP]:
-        #if binimg.rect.y<=height:
-            binimg.rect.y-=10
-    if keys[pygame.K_LEFT]:
-        #if binimg.rect.x>=0:
-            binimg.rect.x-=10
-    if keys[pygame.K_RIGHT]:
-       # if binimg.rect.x<=width:
-            binimg.rect.x+=10
-    imgall.draw(screen)
+    #checking if time>60 seconds
+    totaltime=time.time()-start
+    if totaltime>=60:
+        if score>20:
+            bgd("win_bg.png")
+        else:
+            bgd("lose_bg.png")
+    else:
+        
+        bgd("recycle_bg.jpg")
+        #bin movement
+        keys=pygame.key.get_pressed()
+        if keys[pygame.K_DOWN]:
+            #if binimg.rect.y>=0:
+                binimg.rect.y+=10
+        if keys[pygame.K_UP]:
+            #if binimg.rect.y<=height:
+                binimg.rect.y-=10
+        if keys[pygame.K_LEFT]:
+            #if binimg.rect.x>=0:
+                binimg.rect.x-=10
+        if keys[pygame.K_RIGHT]:
+        # if binimg.rect.x<=width:
+                binimg.rect.x+=10
+        imgall.draw(screen)
     pygame.display.update()
 pygame.quit()
